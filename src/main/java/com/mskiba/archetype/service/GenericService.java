b464e69aeda3636b1e030936d3daa10c7ac314f3
mskiba <cines91@gmail.com>
2014-10-28 15:01:31 +0100

    Moved @Transactional annotation to implementation classes

--- src/main/java/com/mskiba/archetype/service/GenericService.java
+++ src/main/java/com/mskiba/archetype/service/GenericService.java
@@ -2,21 +2,18 @@ package com.mskiba.archetype.service;
 
 import com.mskiba.archetype.dao.GenericDAO;
 import com.mskiba.archetype.model.AbstractModel;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 public interface GenericService<T extends AbstractModel> {
     GenericDAO<T> getBaseDAO();
 
-    @Transactional
     T getById(int id);
 
-    @Transactional
     void save(T entity);
 
-    @Transactional
     void update(T entity);
 
-    @Transactional
     void delete(T entity);
 }
--- src/main/java/com/mskiba/archetype/service/impl/GenericServiceImpl.java
+++ src/main/java/com/mskiba/archetype/service/impl/GenericServiceImpl.java
@@ -2,22 +2,27 @@ package com.mskiba.archetype.service.impl;
 
 import com.mskiba.archetype.model.AbstractModel;
 import com.mskiba.archetype.service.GenericService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public abstract class GenericServiceImpl<T extends AbstractModel> implements GenericService<T> {
 
+    @Transactional
     public T getById(int id) {
         return getBaseDAO().getById(id);
     }
 
+    @Transactional
     public void save(T entity) {
         getBaseDAO().save(entity);
     }
 
+    @Transactional
     public void update(T entity) {
         getBaseDAO().update(entity);
     }
 
+    @Transactional
     public void delete(T entity) {
         getBaseDAO().delete(entity);
     }
